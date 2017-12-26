4bb12c873ac2f4f770d41e4eba7b7ebd5ed76214
xAphex <5391237+xAphex@users.noreply.github.com>
2017-12-26 20:12:04 +0200

    optimized for mobile

---
+++ menubar.js
@@ -0,0 +1,14 @@
+if (document.documentElement.clientWidth < 500) {
+
+	function openNav() {
+		document.getElementById("menubar").style.width = "250px";
+	}
+
+	window.addEventListener('mouseup', function(event){
+		var menubar= document.getElementById("menubar");
+		if (event.target != menubar && event.target.parentNode != menubar){
+			document.getElementById("menubar").style.width = "0";
+		}
+	});
+
+}
